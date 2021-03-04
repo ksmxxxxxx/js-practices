671e3a1762b283f72edf9e577f9de74822b4abe5
ksmxxxxxx <kankichi69@gmail.com>
2021-03-04 22:09:23 +0900

    :construction: functionalization

--- class/note_app.js
+++ class/note_app.js
@@ -4,20 +4,29 @@ import crypto from 'crypto'
 const title = 'Using fs.writeFile() with file descriptors'
 const body = 'When file is a file descriptor, the behavior is almost identical to directly calling fs.write() like:'
 const uuid = crypto.randomBytes(16).toString('hex')
-const file = `${uuid}.json`
+const file = `data/${uuid}.json`
 const charset = 'utf-8'
 
-const note = {
-  uuid: uuid,
-  title: title,
-  body: body
+function Note () {
+  return {
+    uuid: '',
+    title: '',
+    body: ''
+  }
 }
 
-const data = JSON.stringify(note, null, 2)
-
-fs.open(file, 'a', err => {
-  if (err) throw err
-  fs.writeFile(file, data, charset, err => {
+function writeNote (uuid, title, body) {
+  const note = new Note()
+  note.uuid = uuid
+  note.title = title
+  note.body = body
+  const data = JSON.stringify(note, null, 2)
+  fs.open(file, 'a', err => {
     if (err) throw err
+    fs.writeFile(file, data, charset, err => {
+      if (err) throw err
+    })
   })
-})
+}
+
+writeNote(uuid, title, body)
