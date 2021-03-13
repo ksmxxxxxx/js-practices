794efd848d637b6ba86956257137ca88060c96b5
ksmxxxxxx <kankichi69@gmail.com>
2021-03-13 22:21:17 +0900

    :sparkles: Create all the functions need

--- class/note_app.js
+++ class/note_app.js
@@ -1,18 +1,29 @@
 import fs from 'fs'
 import readline from 'readline'
+import minimist from 'minimist'
+import inquirer from 'inquirer'
 import crypto from 'crypto'
 
-// const title = 'Using fs.writeFile() with file descriptors'
-// const body = 'When file is a file descriptor, the behavior is almost identical to directly calling fs.write() like:'
 const uuid = crypto.randomBytes(16).toString('hex')
-const file = `data/${uuid}.json`
+const addNewFile = `data/${uuid}.json`
 const charset = 'utf-8'
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
+const argv = minimist(process.argv.slice(2))
 
 let tempStrageStdinText = ''
+
+if (argv.l) {
+  displayNoteList()
+} else if (argv.r) {
+  selectReferenceNote()
+} else if (argv.d) {
+  selectDeleteNote()
+} else {
+  addNewNote()
+}
 
 function Note () {
   return {
@@ -26,9 +37,9 @@ function writeNote (uuid, body) {
   note.uuid = uuid
   note.body = body
   const data = JSON.stringify(note, null, 2)
-  fs.open(file, 'a', err => {
+  fs.open(addNewFile, 'a', err => {
     if (err) throw err
-    fs.writeFile(file, data, charset, err => {
+    fs.writeFile(addNewFile, data, charset, err => {
       if (err) throw err
     })
   })
@@ -42,36 +53,101 @@ function addNewNote () {
   })
 }
 
-function filterJsonFiles (files) {
+function readDirectoryOfData (path) {
+  return fs.readdirSync(path)
+}
+
+function filterJsonFiles () {
+  const files = readDirectoryOfData('data')
   return files.filter(file => { return /.*\.json$/.test(file) })
 }
 
-function pickFirstLineNote (files) {
+function readFile (file) {
+  return fs.readFileSync(`data/${file}`, charset)
+}
+
+function parseJsonFile (file) {
+  return JSON.parse(readFile(file))
+}
+
+function createAllNoteData () {
+  const files = filterJsonFiles()
   return files.map(file => {
-    return JSON.parse(fs.readFileSync(`data/${file}`, charset)).body.split('\n')[0]
+    return parseJsonFile(file)
+  })
+}
+
+function pickUuidOfNote () {
+  const files = createAllNoteData()
+  return files.map(file => {
+    return parseJsonFile(file).uuid
+  })
+}
+
+function pickFirstLineOfNote () {
+  const files = createAllNoteData()
+  return files.map(file => {
+    return file.body.split('\n')[0]
   })
 }
 
 function displayNoteList () {
-  fs.readdir('data', (err, files) => {
-    if (err) throw err
-    const jsonFiles = filterJsonFiles(files)
-    const firstLineNote = pickFirstLineNote(jsonFiles).join('\n')
-    console.log(firstLineNote)
+  const firstLineOfNote = pickFirstLineOfNote()
+  console.log(firstLineOfNote.join('\n'))
+  process.exit(0)
+}
+
+function createSelectNoteList () {
+  const data = createAllNoteData()
+  return data.map(hash => {
+    const newHash = {}
+    newHash.name = hash.body.split('\n')[0]
+    newHash.value = `${hash.uuid}`
+    return newHash
+  })
+}
+
+function selectReferenceNote () {
+  const targetList = createSelectNoteList()
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'uuid',
+      message: 'Choose reference note to ...',
+      choices: [...targetList]
+    }
+  ]).then(answers => {
+    console.log(referenceNoteBody(answers.uuid))
+    process.exit(0)
+  }).catch(err => {
+    console.log(err)
     process.exit(0)
   })
 }
 
-displayNoteList()
-// addNewNote()
+function referenceNoteBody (id) {
+  return JSON.parse(fs.readFileSync(`data/${id}.json`, charset)).body
+}
 
-// 標準入力したテキストをオブジェクトに登録
+function selectDeleteNote () {
+  const targetList = createSelectNoteList()
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'uuid',
+      message: 'Choose delete note to ...',
+      choices: [...targetList]
+    }
+  ]).then(answers => {
+    deleteNote(answers.uuid)
+    console.log(`data/${answers.uuid}.json` + ' was deleted')
+    process.exit(0)
+  }).catch(err => {
+    console.log(err)
+    process.exit(0)
+  })
+}
 
-// rl.question('please input text for title of new note: ', (answer) => {
-//   console.log(`title: ${answer}`)
-// })
-// ディレクトリの中のファイルを取得する
-// fs.readdir('data', (err, file) => {
-//   if (err) throw err
-//   console.log(file)
-// })
+function deleteNote (id) {
+  return fs.unlinkSync(`data/${id}.json`)
+}
